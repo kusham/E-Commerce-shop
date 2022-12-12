c43452d43572289e1fd9d406b7260ede535d3ca2
kusham <kusham1998@gmail.com>
2022-12-13 02:12:21 +0530

    add register controller to route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const { createUser } = require('../controllers/userControllers');
 
 const router = express.Router();
 
-router.post("/register")
+router.post("/register", createUser)
 module.exports = router;
