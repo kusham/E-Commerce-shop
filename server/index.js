a6e08d4a401702f626aa6d8b1abe55a8ebf810d5
kusham <kusham1998@gmail.com>
2022-12-13 02:47:26 +0530

    add body parser and route

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const dbConnect  = require("./config/dbConnect");
+const dbConnect = require("./config/dbConnect");
+const authRoute = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
-    dbConnect(); 
+  console.log(`Server is running on PORT ${PORT}`);
+  dbConnect();
 });
- 
+
+app.use("/api/user", authRoute);
