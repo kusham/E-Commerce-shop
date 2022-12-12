92fb6f06a4a78b29cdaae38fba1f1d780ba47a47
kusham <kusham1998@gmail.com>
2022-12-13 03:01:37 +0530

    add asyncHandler

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,6 +1,8 @@
 const userModal = require("../models/userModal");
+const asyncHandler = require("express-async-handler");
 
-module.exports.createUser = async (req, res) => {
+
+module.exports.createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await userModal.findOne({ email: email });
   if (!findUser) {
@@ -8,9 +10,6 @@ module.exports.createUser = async (req, res) => {
     const newUser = await userModal.create(req.body);
     res.json(newUser);
   } else {
-    res.json({
-      success: false,
-      message: "User already exists",
-    });
+   throw new Error("User already exists")
   }
-};
+});
