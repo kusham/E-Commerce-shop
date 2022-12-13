f840722793482de01eb99bdbfce51d558517f53b
kusham <kusham1998@gmail.com>
2022-12-14 03:29:25 +0530

    single user rote create

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createUser, loginUser, getAllUsers } = require('../controllers/userControllers');
+const { createUser, loginUser, getAllUsers, getSingleUser } = require('../controllers/userControllers');
 
 const router = express.Router();
 
 router.post("/register", createUser)
 router.post("/login", loginUser)
 router.get("/allUsers", getAllUsers)
+router.get("/singleUse", getSingleUser)
+
 
 module.exports = router; 
