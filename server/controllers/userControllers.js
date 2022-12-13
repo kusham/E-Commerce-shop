62dfcbc98a7b1006fb6068e0bf6cdc7220e8a1f2
kusham <kusham1998@gmail.com>
2022-12-14 03:14:42 +0530

    create login controller

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,7 +1,6 @@
 const userModal = require("../models/userModal");
 const asyncHandler = require("express-async-handler");
 
-
 module.exports.createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await userModal.findOne({ email: email });
@@ -10,6 +9,19 @@ module.exports.createUser = asyncHandler(async (req, res) => {
     const newUser = await userModal.create(req.body);
     res.json(newUser);
   } else {
-   throw new Error("User already exists")
+    throw new Error("User already exists");
+  }
+});
+
+module.exports.loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  // check whether user exists
+
+  const findUser = await userModal.findOne({ email: email });
+  if (findUser && (await findUser.isPasswordMatched(password))) {
+    res.json(findUserF);
+  } else {
+    throw Error("Invalid Credentials");
   }
 });
