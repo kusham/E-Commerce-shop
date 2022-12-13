64f3af4b1dd2aa9d362d1fcbfae559d3d0d81abe
kusham <kusham1998@gmail.com>
2022-12-14 03:27:20 +0530

    all user route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createUser, loginUser } = require('../controllers/userControllers');
+const { createUser, loginUser, getAllUsers } = require('../controllers/userControllers');
 
 const router = express.Router();
 
 router.post("/register", createUser)
 router.post("/login", loginUser)
+router.get("/allUsers", getAllUsers)
 
 module.exports = router; 
