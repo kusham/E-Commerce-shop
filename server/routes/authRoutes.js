077c3be56b3ff61c192f7b70aa1248b39f60e906
kusham <kusham1998@gmail.com>
2022-12-14 03:15:26 +0530

    login route create

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { createUser } = require('../controllers/userControllers');
+const { createUser, loginUser } = require('../controllers/userControllers');
 
 const router = express.Router();
 
 router.post("/register", createUser)
+router.post("/login", loginUser)
+
 module.exports = router; 
