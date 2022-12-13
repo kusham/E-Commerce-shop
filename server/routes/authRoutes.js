e55167507f446d36369bb438600699b0ae3d723f
kusham <kusham1998@gmail.com>
2022-12-14 03:32:45 +0530

    delete user rote

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const { createUser, loginUser, getAllUsers, getSingleUser } = require('../controllers/userControllers');
+const express = require("express");
+const {
+  createUser,
+  loginUser,
+  getAllUsers,
+  getSingleUser,
+  deleteUser,
+} = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router.post("/register", createUser)
-router.post("/login", loginUser)
-router.get("/allUsers", getAllUsers)
-router.get("/:id", getSingleUser)
+router.post("/register", createUser);
+router.post("/login", loginUser);
+router.get("/allUsers", getAllUsers);
+router.get("/:id", getSingleUser);
+router.delete("/:id", deleteUser);
 
-
-module.exports = router; 
+module.exports = router;
