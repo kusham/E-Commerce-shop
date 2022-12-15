2e17d16f2ecd056f6f23795c9c7b2970357060b3
kusham <kusham1998@gmail.com>
2022-12-15 12:18:16 +0530

    changed after

---
+++ server/middleware/middleware.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const userModal = require("../models/userModal");
+
+const authMiddleware = asyncHandler(async (req, res, next) => {
+  let token;
+  if (req?.header?.authorization?.startsWith("Bearer")) {
+    token = req.header.authorization.split(" ")[1];
+    try {
+      if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModal.findById(decoded?.id);
+        req.user = user;
+        next();
+      }
+    } catch (error) {
+      throw new Error("Not Authorized, token expired. Please login again.");
+    }
+  } else {
+    throw new Error("There is no token attached to header.");
+  }
+});
+
+const isAdmin = asyncHandler(async (req, res, next) => {
+  const { email } = req.user;
+  const adminUser = await userModal.findOne({ email });
+  if (adminUser?.role !== "admin") {
+    throw new Error("You are not an admin");
+  } else {
+    next();
+  }
+});
+
+module.exports = { authMiddleware, isAdmin };
