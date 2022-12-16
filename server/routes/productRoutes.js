d0a905209f9fdfb0b79643b2bf10cc7538ee255b
kusham <kusham1998@gmail.com>
2022-12-16 14:09:46 +0530

    add getAllProduct route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { createProduct, getProduct } = require("../controllers/productController");
+const { createProduct, getProduct, getAllProduct } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/:id", getProduct);
+router.get("/all-product", getAllProduct);
+
 
 
 module.exports = router;
