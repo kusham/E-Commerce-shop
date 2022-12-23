cbeefd700cf6dd4efab1459ba7cb1fa32aa1d03c
kusham <kusham1998@gmail.com>
2022-12-23 14:27:45 +0530

    import slugify and mogan

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
-
+const cookieParser = require("cookie-parser");
+const morgan = require('morgan');
+const slugify = require('slugify');
 
 const dbConnect = require("./config/dbConnect");
 const authRoute = require("./routes/authRoutes");
 const productRoute = require("./routes/productRoutes");
 const { notFound, errorHandler } = require("./middleware/errorHandler");
 
-
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -28,3 +29,4 @@ app.use("/api/product", productRoute);
 app.use(notFound);
 app.use(errorHandler);
 
+morgan;
