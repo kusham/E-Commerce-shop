0a489c52239fa46fd6dca2552090a5c0b9405a0e
kusham <kusham1998@gmail.com>
2022-12-25 12:00:29 +0530

    updateBlog route

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createBlog } = require("../controllers/blogController");
+const { createBlog, updateBlog } = require("../controllers/blogController");
 const { isAdmin, authMiddleware } = require("../middleware/middleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createBlog);
+router.put("/:id", authMiddleware, updateBlog);
+
 
 module.exports = router;
