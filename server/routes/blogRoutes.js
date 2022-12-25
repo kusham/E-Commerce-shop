7c978524c2fc1f281b390116877306dfb7dc7162
kusham <kusham1998@gmail.com>
2022-12-25 12:03:32 +0530

    getBlog route

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { createBlog, updateBlog } = require("../controllers/blogController");
+const { createBlog, updateBlog, getBlog } = require("../controllers/blogController");
 const { isAdmin, authMiddleware } = require("../middleware/middleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createBlog);
 router.put("/:id", authMiddleware, updateBlog);
+router.get("/:id", getBlog);
+
 
 
 module.exports = router;
