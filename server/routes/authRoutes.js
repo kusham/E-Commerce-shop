1186c2fe17de6d9ea7e11a485a5c3e73bb2a8351
kusham <kusham1998@gmail.com>
2022-12-25 19:05:19 +0530

    add middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,22 +9,21 @@ const {
   blockUser,
   unBlockUser,
   handleRefreshToken,
+  updatePassword,
 } = require("../controllers/userControllers");
-const {authMiddleware, isAdmin} = require("../middleware/middleware");
+const { authMiddleware, isAdmin } = require("../middleware/middleware");
 
 const router = express.Router();
 
 router.post("/register", createUser);
+router.put("/password", authMiddleware, updatePassword);
 router.post("/login", loginUser);
 router.get("/all-users", isAdmin, getAllUsers);
 router.get("/refresh", handleRefreshToken);
-router.get("/:id", getSingleUser);
+router.get("/:id", authMiddleware, isAdmin, getSingleUser);
 router.delete("/:id", deleteUser);
-router.put("/edit-user", updateUser);
-router.put("/block-user/:id", blockUser);
-router.put("/unBlock-user/:id", unBlockUser);
-
-
-
+router.put("/edit-user", authMiddleware, updateUser);
+router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
+router.put("/unBlock-user/:id", authMiddleware, isAdmin, unBlockUser);
 
 module.exports = router;
