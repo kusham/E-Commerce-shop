527788d977d11c08b06949b52a39efe5a426b247
kusham <kusham1998@gmail.com>
2022-12-25 11:20:41 +0530

    forget-password-token route add

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -10,6 +10,7 @@ const {
   unBlockUser,
   handleRefreshToken,
   updatePassword,
+  forgetPasswordToken,
 } = require("../controllers/userControllers");
 const { authMiddleware, isAdmin } = require("../middleware/middleware");
 
@@ -17,11 +18,13 @@ const router = express.Router();
 
 router.post("/register", createUser);
 router.put("/password", authMiddleware, updatePassword);
+router.post("/forget-password-token", forgetPasswordToken);
 router.post("/login", loginUser);
 router.get("/all-users", isAdmin, getAllUsers);
 router.get("/refresh", handleRefreshToken);
 router.get("/:id", authMiddleware, isAdmin, getSingleUser);
 router.delete("/:id", deleteUser);
+
 router.put("/edit-user", authMiddleware, updateUser);
 router.put("/block-user/:id", authMiddleware, isAdmin, blockUser);
 router.put("/unBlock-user/:id", authMiddleware, isAdmin, unBlockUser);
