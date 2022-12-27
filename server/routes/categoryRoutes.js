d7ff892775de6e70d61385ddab43f3b82d0fe066
kusham <kusham1998@gmail.com>
2022-12-27 14:10:21 +0530

    updateCategory route

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createCategory } = require("../controllers/productCategoryController");
+const { createCategory, updateCategory } = require("../controllers/productCategoryController");
 const { isAdmin, authMiddleware } = require("../middleware/middleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createCategory);
+router.put("/:id", authMiddleware, updateCategory);
+
 
 module.exports = router;
