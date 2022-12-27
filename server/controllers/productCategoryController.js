7088b6c2e4d4a668eb0ba7de7237567edf161a58
kusham <kusham1998@gmail.com>
2022-12-27 14:10:10 +0530

    updateCategory controller

--- server/controllers/productCategoryController.js
+++ server/controllers/productCategoryController.js
@@ -1,13 +1,29 @@
 const asyncHandler = require("express-async-handler");
 const categoryModel = require("../models/productCategoryModel");
-
+const validateMongodbId = require("../utils/validateMongodbId");
 
 // create category
 module.exports.createCategory = asyncHandler(async (req, res) => {
-    try {
-      const newCategory = await categoryModel.create(req.body);
-      res.json(newCategory);
-    } catch (error) {
-      throw Error(error);
-    }
-  });
+  try {
+    const newCategory = await categoryModel.create(req.body);
+    res.json(newCategory);
+  } catch (error) {
+    throw Error(error);
+  }
+});
+
+// update category
+module.exports.updateCategory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+  try {
+    const updatedCategory = await categoryModel.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedCategory);
+  } catch (error) {
+    throw Error(error);
+  }
+});
