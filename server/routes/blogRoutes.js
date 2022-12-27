11839c575614f94fafc644188e4072107efb05c0
kusham <kusham1998@gmail.com>
2022-12-27 13:52:10 +0530

    disLikeBlog route

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const { createBlog, updateBlog, getBlog, getAllBlog, deleteBlog, likeBlog } = require("../controllers/blogController");
+const { createBlog, updateBlog, getBlog, getAllBlog, deleteBlog, likeBlog, disLikeBlog } = require("../controllers/blogController");
 const { isAdmin, authMiddleware } = require("../middleware/middleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createBlog);
+router.put("/likes", authMiddleware, isAdmin, likeBlog);
+router.put("/dislikes", authMiddleware, isAdmin, disLikeBlog);
+
+
 router.put("/:id", authMiddleware, updateBlog);
 router.get("/:id", getBlog);
 router.get("/", getAllBlog);
 router.delete("/:id", authMiddleware, deleteBlog);
-router.put("/likes", authMiddleware, isAdmin, likeBlog);
 
 
 
