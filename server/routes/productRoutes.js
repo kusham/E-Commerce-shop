3cf3b647b9a22b48c4c6a8c75036336492bad32c
kusham <kusham1998@gmail.com>
2023-01-02 15:06:05 +0530

    wish list route add

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { createProduct, getProduct, getAllProduct, updateProduct, deleteProduct } = require("../controllers/productController");
+const { createProduct, getProduct, getAllProduct, updateProduct, deleteProduct, addToWishList } = require("../controllers/productController");
 const { isAdmin } = require("../middleware/middleware");
 
 const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/:id", getProduct);
+router.put("/wishlist", addToWishList);
+
 router.put("/:id", updateProduct);
 router.delete("/:id", isAdmin, deleteProduct);
 router.get("/", getAllProduct);
