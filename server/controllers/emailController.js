5bd443270bbf4a6e7153f4b521e57dad232310f3
kusham <kusham1998@gmail.com>
2023-02-18 20:57:35 +0530

    typo

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-const asyncHandler = require("express-expressAsyncHandler");
+const asyncHandler = require("express-async-handler");
 
 const sendEmail = asyncHandler(async (data, req, res) => {
   let transporter = nodemailer.createTransport({
