a313ded94a59ad52e2ac3df60adc44143d78979b
kusham <kusham1998@gmail.com>
2023-02-21 19:36:18 +0530

    getAllCoupon route

--- server/routes/couponRoutes.js
+++ server/routes/couponRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { createCoupon } = require("../controllers/couponController");
+const { createCoupon, getAllCoupon } = require("../controllers/couponController");
 const { isAdmin, authMiddleware } = require("../middleware/middleware");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createCoupon);
+router.get("/", authMiddleware, isAdmin, getAllCoupon);
+
 
 
 module.exports = router;
