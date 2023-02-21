0f9eebfdc271b87b2a40424656c7d7ddd72cc158
kusham <kusham1998@gmail.com>
2023-02-21 19:39:32 +0530

    updateCoupon controller

--- server/controllers/couponController.js
+++ server/controllers/couponController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const couponModel = require("../models/couponModel");
+const validateMongodbId = require("../utils/validateMongodbId");
 
 // create coupon
 module.exports.createCoupon = asyncHandler(async (req, res) => {
@@ -16,6 +17,20 @@ module.exports.getAllCoupon = asyncHandler(async (req, res) => {
   try {
     const allCoupon = await couponModel.find();
     res.json(allCoupon);
+  } catch (error) {
+    throw Error(error);
+  }
+});
+
+// update coupon
+module.exports.updateCoupon = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbId(id);
+  try {
+    const updatedCoupon = await couponModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCoupon);
   } catch (error) {
     throw Error(error);
   }
