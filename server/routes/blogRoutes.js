4fd920192b60cdead0949e5fea290f423870d754
kusham <kusham1998@gmail.com>
2023-02-24 20:27:49 +0530

    uploadImages route

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -7,12 +7,25 @@ const {
   deleteBlog,
   likeBlog,
   disLikeBlog,
+  uploadImages,
 } = require("../controllers/blogController");
 const { isAdmin, authMiddleware } = require("../middleware/middleware");
+const {
+  uploadPhoto, blogImageResize,
+} = require("../middleware/uploadImages");
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createBlog);
+router.put(
+  "/upload/:id",
+  authMiddleware,
+  isAdmin,
+  uploadPhoto.array("images", 2),
+  blogImageResize,
+  uploadImages
+);
+
 router.put("/likes", authMiddleware, isAdmin, likeBlog);
 router.put("/dislikes", authMiddleware, isAdmin, disLikeBlog);
 
