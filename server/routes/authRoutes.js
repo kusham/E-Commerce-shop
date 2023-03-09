3614e7acc36b15bf8f5bfb5b2279a9a21abe690a
kusham <kusham1998@gmail.com>
2023-03-09 20:29:11 +0530

    createOrder route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -19,6 +19,7 @@ const {
   getUserCart,
   emptyCart,
   applyCoupon,
+  createOrder,
 } = require("../controllers/userControllers");
 const { authMiddleware, isAdmin } = require("../middleware/middleware");
 
@@ -31,7 +32,9 @@ router.put("/reset-password/:token", resetPassword);
 router.post("/login", loginUser);
 router.post("/admin-login", adminLogin);
 router.post("/cart", userCart);
-router.post("/cart/apply-coupon", isAdmin, applyCoupon);
+router.post("/cart/apply-coupon", authMiddleware, applyCoupon);
+router.post("/cart/cash-order", authMiddleware, createOrder);
+
 
 router.get("/all-users", isAdmin, getAllUsers);
 router.get("/refresh", handleRefreshToken);
