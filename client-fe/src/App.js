9463fe2d94af293c4c3affe49ee42e7559fd7f65
kusham <kusham1998@gmail.com>
2023-03-14 12:13:29 +0530

    add signup route

--- client-fe/src/App.js
+++ client-fe/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="login" element={<Login />} />
+          <Route path="signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </React.Fragment>
