fa08e9242c08ef0dc32e52cbbbc4b85a9f7941dc
kusham <kusham1998@gmail.com>
2023-03-17 12:29:47 +0530

    add home route

--- client-fe/src/App.js
+++ client-fe/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
         <Routes>
+          <Route path="login" element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
         </Routes>
