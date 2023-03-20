0ce0dcc1646d7420093bcd90eebc9614b289e5c7
kusham <kusham1998@gmail.com>
2023-03-20 13:05:05 +0530

    add blog route

--- client-fe/src/App.js
+++ client-fe/src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
           <Route path="login" element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
+
+          <Route path="blogs" element={<Blog />} />
         </Routes>
       </BrowserRouter>
     </React.Fragment>
